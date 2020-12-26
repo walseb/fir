35d7913014fce5619825e6ac8cbcb406ebf7d6e4
sheaf <sam.derbyshire@gmail.com>
2020-12-26 15:54:15 +0100

    add tab autocompletion to scene chooser

--- fir-examples/examples/apps/FIR/Examples/RayTracing/Scenes.hs
+++ fir-examples/examples/apps/FIR/Examples/RayTracing/Scenes.hs
@@ -1,13 +1,23 @@
+{-# LANGUAGE BlockArguments    #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module FIR.Examples.RayTracing.Scenes
   ( scenes, chooseScene ) where
 
+-- base
+import Data.Maybe
+  ( fromMaybe, listToMaybe )
+import System.Environment
+  ( getArgs )
+
+-- haskeline
+import qualified System.Console.Haskeline as Haskeline
+
 -- text-short
 import Data.Text.Short
   ( ShortText )
 import qualified Data.Text.Short as ShortText
-  ( pack )
+  ( dropAround, isPrefixOf, pack, unpack )
 
 -- unordered-containers
 import Data.HashMap.Strict
@@ -33,18 +43,39 @@ scenes = HashMap.fromList
 
 chooseScene :: IO Scene
 chooseScene = do
-  putStrLn "Choose a scene."
-  putStrLn 
-    ( "Available scenes:\n" <> show ( HashMap.keys scenes ) )
-  go
-  
-    where
-      go :: IO Scene
-      go = do
-        name <- getLine
-        case HashMap.lookup ( ShortText.pack name ) scenes of
-          Nothing -> do
-            putStrLn ( "Could not find a scene named " <> name <> ".")
-            putStrLn ( "Available scenes are:\n" <> show ( HashMap.keys scenes ) )
-            go
-          Just scene -> pure scene
+  args <- getArgs
+  Haskeline.runInputT haskelineSettings do
+    inTerminal <- Haskeline.haveTerminalUI
+    if inTerminal
+    then do
+      Haskeline.outputStrLn "Choose a scene."
+      Haskeline.outputStrLn
+        ( "Available scenes:\n" <> show ( HashMap.keys scenes ) )
+      queryUserForScene
+    else pure $
+      fromMaybe cornellBox -- default scene
+        ( ( `HashMap.lookup` scenes ) . ShortText.dropAround ( == ' ' ) . ShortText.pack =<< listToMaybe args )
+
+  where
+    queryUserForScene :: Haskeline.InputT IO Scene
+    queryUserForScene = do
+      mbLine <- Haskeline.getInputLine "> "
+      case ( `HashMap.lookup` scenes ) . ShortText.dropAround ( == ' ' ) . ShortText.pack =<< mbLine of
+        Nothing -> do
+          Haskeline.outputStrLn "Could not parse scene name."
+          Haskeline.outputStrLn ( "Available scenes are:\n" <> show ( HashMap.keys scenes ) )
+          queryUserForScene
+        Just scene -> pure scene
+
+haskelineSettings :: Haskeline.Settings IO
+haskelineSettings = Haskeline.Settings
+  { Haskeline.complete       = Haskeline.completeWord Nothing " \t" ( pure . completeSceneName )
+  , Haskeline.historyFile    = Nothing
+  , Haskeline.autoAddHistory = False
+  }
+
+completeSceneName :: String -> [ Haskeline.Completion ]
+completeSceneName str
+  = map ( Haskeline.simpleCompletion . ShortText.unpack )
+  . filter ( ShortText.pack str `ShortText.isPrefixOf` )
+  $ HashMap.keys scenes
