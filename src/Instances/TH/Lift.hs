6245f82596889d49ee033e17b56557cb39286f9c
Tristan Cacqueray <tdecacqu@redhat.com>
2021-10-22 12:23:39 +0000

    Bump text-short to 0.1.4

--- src/Instances/TH/Lift.hs
+++ src/Instances/TH/Lift.hs
@@ -19,20 +19,6 @@ import Data.Set
 import qualified Data.Set as Set
   ( fromList, toList )
 
--- text-short
-import Data.Text.Short
-  ( ShortText )
-import qualified Data.Text.Short as ShortText
-  ( pack, unpack )
-
---------------------------------------------------------
-
-instance Lift ShortText where
-  lift      t = [|  ShortText.pack  $(lift      $ ShortText.unpack t)  |]
-#if MIN_VERSION_template_haskell(2,16,0)
-  liftTyped t = [|| ShortText.pack $$(liftTyped $ ShortText.unpack t) ||]
-#endif
-
 instance (Ord a, Lift a) => Lift (Set a) where
   lift      t = [|  Set.fromList  $(lift      $ Set.toList t)  |]
 #if MIN_VERSION_template_haskell(2,16,0)
