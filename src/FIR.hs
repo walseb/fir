8261a7eba0fb1714d6ebfb9d11d6cd5146753e62
Sebastian <s.walinder@gmail.com>
2025-10-23 23:59:44 +0200

    bump

--- src/FIR.hs
+++ src/FIR.hs
@@ -220,6 +220,9 @@ module FIR
   -- known
   , Data.Type.Known.Known(..)
   , Data.Type.Known.knownValue
+
+  -- Re-export of prelude and related functions
+  , module FIR.Prelude
   ) where
 
 -- base
